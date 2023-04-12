b4e849988e3bba0bb138d5a7c15d575354bd29b9
sohan-r30 <sohanakondo139@gmail.com>
2023-04-12 16:44:24 +0600

    readme.md file added

---
+++ README.md
@@ -0,0 +1,12 @@
+# Job Hunting
+
+* This is a site for applying in different types of jobs
+* It is a single page application
+* It have some section for applying job
+* We can apply on this jobs on dynamic routing page
+* Applieed jobs data are stored in local storage
+* we can see all applied jobs by filtering 
+* It's also has a statistics page where are stored the previous assignment data
+
+
+[LiveLink] (https://sohan-r30-job-hunting-project.netlify.app/ "project live link")
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -16,15 +16,35 @@ const Blog = () => {
             <div className='text-justify border-2 w-3/4 mx-auto m-10 p-10 rounded-lg break-words'>
                 <div>
                     <p className='text-2xl font-bold'>1. When should you use context API?</p>
+                    <p className='text-secondaryColor pl-4'>context api is used for share data between component.
+                        we can share data by using context api from parent 
+                        component to nested children components.when we need
+                        to pass data by props in 6/7 or more inner nested component
+                        then we can use context api for pass data directly to that components
+                    </p>
                 </div>
                 <div className='mt-10'>
                     <p className='text-2xl font-bold'>2. What is a custom hook?</p>
+                    <p className='text-secondaryColor pl-4'>
+                        custom hook are reuseble functions. custom hooks names are start with use keyword.
+                        to create a custom hook, you define a function that uses one or more of the existing
+                         react hooks and then export that function.
+                    </p>
                 </div>
                 <div className='mt-10'>
                     <p className='text-2xl font-bold'>3. What is useRef?</p>
+                    <p className='text-secondaryColor pl-4'>
+                        useRef is a react hook which is a function that returns a mutable ref object . 
+                        this ref objectcan be used to keep track of a value across renders without triggering 
+                        a re-render.
+                    </p>
                 </div>
                 <div className='mt-10'>
                     <p className='text-2xl font-bold'>4. What is useMemo</p>
+                    <p className='text-secondaryColor pl-4'>
+                        useMemo is a react hook which is allows you to memoize expensive functions 
+                        so that you can avoid calling them on every render.
+                    </p>
                 </div>
         </div>
         <div className='mt-32 w-full bg-primaryColor 2xl:px-72'>
